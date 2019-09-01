77ded3bfab58e71694fd6cb218ca0371fd5cddef
ahdyxem <ahdyxem163.com>
2019-09-01 15:33:37 +0800

    type: add
    scope:  create springCloud interface project
    
    subject:  springCloud interface project

--- springCloud/springCloud-item/springCloud-service/src/main/java/com/ahdy/xem/ServiceApplication.java
+++ springCloud/springCloud-item/springCloud-service/src/main/java/com/ahdy/xem/ServiceApplication.java
@@ -2,13 +2,12 @@ package com.ahdy.xem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableEurekaServer
-public class SpringCloudEurekaApplication {
-
+@EnableDiscoveryClient
+public class ServiceApplication {
     public static void main(String[] args) {
-        SpringApplication.run(SpringCloudEurekaApplication.class, args);
+        SpringApplication.run(ServiceApplication.class, args);
     }
 }
