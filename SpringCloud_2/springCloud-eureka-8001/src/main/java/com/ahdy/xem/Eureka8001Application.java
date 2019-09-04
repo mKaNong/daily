5d1a3741fc62768fffe8784e8d811f401ce554fb
ahdyxem <ahdyxem163.com>
2019-09-04 22:48:06 +0800

    type: add
    scope:  create springCloud2 project
    
    subject:  springCloud2 project

---
+++ SpringCloud_2/springCloud-eureka-8001/src/main/java/com/ahdy/xem/Eureka8001Application.java
@@ -0,0 +1,13 @@
+package com.ahdy.xem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class Eureka8001Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Eureka8001Application.class, args);
+    }
+}
---
+++ SpringCloud_2/springCloud-eureka-8002/src/main/java/com/ahdy/xem/Eureka8002Application.java
@@ -0,0 +1,13 @@
+package com.ahdy.xem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class Eureka8002Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Eureka8002Application.class, args);
+    }
+}
